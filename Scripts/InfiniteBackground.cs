bd567bb1041a81b8c262ba09dd27186934893066
CyberButton <87766623307Kk>
2023-05-11 14:18:09 +0600

    added offset animation for the background

---
+++ Scripts/InfiniteBackground.cs
@@ -0,0 +1,20 @@
+
+using UnityEngine;
+
+public class InfiniteBackground : MonoBehaviour
+{
+    private MeshRenderer mRenderer;
+
+    public float offsetSpeed = 1f;
+
+    private void Awake() {
+        mRenderer = GetComponent<MeshRenderer>();
+    }
+
+    //change the offset of the mesh to make it look like player is moving
+    private void Update() {
+        // only moving in x-axis so y = 0
+        Vector2 offset = new Vector2(offsetSpeed * Time.deltaTime, 0);
+        mRenderer.material.mainTextureOffset += offset;
+    }
+}
