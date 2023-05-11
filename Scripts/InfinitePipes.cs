31bb7930d7c016526717de460f0380ecaade7be9
CyberButton <87766623307Kk>
2023-05-11 19:05:13 +0600

    added infinte obstacle spawner

---
+++ Scripts/InfinitePipes.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfinitePipes : MonoBehaviour
+{
+    //refrence to pipes
+    public GameObject prefab;
+    
+    public float spawnRate = 1f;
+    public float minHeight = -1f;
+    public float maxHeight = 1f;
+
+    //create new obstacles repetadely 
+    private void OnEnable() {
+        InvokeRepeating(nameof(CreateObstacle), spawnRate, spawnRate);
+    }
+
+    private void OnDisable() {
+        CancelInvoke(nameof(CreateObstacle));
+    }
+
+    private void CreateObstacle() {
+        //Quaternion.identity == no rotation
+        GameObject pipes = Instantiate(prefab, transform.position, Quaternion.identity);        
+        pipes.transform.position += Vector3.up * Random.Range(minHeight, maxHeight);
+    }
+}
